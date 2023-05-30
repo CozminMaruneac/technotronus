db6c5864dfc2b8996c81a6f17c9d8e10c5d3d87c
Cosmin <cosmin.maruneac@assist.ro>
2023-05-30 16:43:11 +0300

    Added cors config

---
+++ src/main/java/com/usv/technotronus/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.usv.technotronus.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    public static final int MAX_AGE_SECS = 3600;
+
+    private static final String ALLOWED_ORIGINS = "http://localhost:3000";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins(ALLOWED_ORIGINS)
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(MAX_AGE_SECS);
+    }
+
+}
